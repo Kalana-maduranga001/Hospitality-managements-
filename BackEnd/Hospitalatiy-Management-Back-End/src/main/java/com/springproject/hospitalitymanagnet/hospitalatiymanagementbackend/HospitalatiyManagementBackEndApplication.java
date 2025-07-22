04fd2de3ea8a002185053e6dc9a803a3ba50b358
Kalana-maduranga001 <kalanamadu2000@gmail.com>
2025-07-22 15:47:01 +0530

    Added full project including BackEnd

---
+++ BackEnd/Hospitalatiy-Management-Back-End/src/main/java/com/springproject/hospitalitymanagnet/hospitalatiymanagementbackend/HospitalatiyManagementBackEndApplication.java
@@ -0,0 +1,15 @@
+package com.springproject.hospitalitymanagnet.hospitalatiymanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalatiyManagementBackEndApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HospitalatiyManagementBackEndApplication.class, args);
+	}
+
+}
+
---
+++ BackEnd/Hospitalatiy-Management-Back-End/src/test/java/com/springproject/hospitalitymanagnet/hospitalatiymanagementbackend/HospitalatiyManagementBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.hospitalitymanagnet.hospitalatiymanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalatiyManagementBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
